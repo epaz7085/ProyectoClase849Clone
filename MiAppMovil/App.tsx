96c200da4544edf6c4acf9adf098524ed6fe5aed
Maria Jose Salinas <overlandwebmaster@gmail.com>
2026-05-04 19:47:19 -0600

    - creacion e implementacion de nuevo componente reutilizable CustomInput - instalacion de libreria para el uso de iconos

--- MiAppMovil/App.tsx
+++ MiAppMovil/App.tsx
@@ -1,12 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import CustomButton from "./src/components/CustomButton";
+import CustomInput from "./src/components/CustomInput";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      
+      <CustomInput 
+      type={"number"} 
+      placeholder={""} 
+      value={""} 
+      onChange={function (text: string): void {} }
+      />
+
+
       <CustomButton
         title={"App"}
         onPress={() => {
